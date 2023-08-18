cfd37ccc651f4d2c8e5bdbe6986aa178bb7d6a5c
nickburczyk <nick.burczyk@gmail.com>
2023-08-18 08:55:16 +0200

    chore: rename and index assets

---
+++ assets/img/index.js
@@ -0,0 +1,21 @@
+import avatar from './Avatar.png';
+import bruschetta from './Bruschetta.png';
+import deliveryVan from './DeliveryVan.png';
+import greekSalad from './GreekSalad.png';
+import grilledFish from './GrilledFish.png';
+import heroImage from './HeroImage.png';
+import lemonDessert from './LemonDessert.png';
+import logo from './Logo.png';
+import pasta from './Pasta.png';
+
+export {
+  avatar,
+  bruschetta,
+  deliveryVan,
+  greekSalad,
+  grilledFish,
+  heroImage,
+  lemonDessert,
+  logo,
+  pasta,
+}
